a9d001827f99c73b57c40aec20f043510420e35e
ndkhanhs_m01 <mainver01@gmail.com>
2024-12-01 13:40:27 +0700

    Editor define symbols

--- Assets/MLib/Debug/Editor/MDebugWindow.cs
+++ Assets/MLib/Debug/Editor/MDebugWindow.cs
@@ -3,6 +3,10 @@ using UnityEditor;
 using UnityEditor.AnimatedValues;
 using UnityEngine;
 using Newtonsoft.Json;
+using Debug = UnityEngine.Debug;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Compilation;
 
 
 namespace MLib
@@ -10,6 +14,9 @@ namespace MLib
     
     public class MDebugWindow : EditorWindow
     {
+        private SerializedObject soThis;
+        [SerializeField] private EditorConfigSO editorConfig;
+
         GUIStyle titleStyle;
 
         #region Button style
@@ -31,6 +38,11 @@ namespace MLib
         GUIStyle styleDataText = new();
         #endregion
 
+        #region Define symbols
+        private BuildTargetGroup buildTargetGroup = BuildTargetGroup.Android;
+        private Dictionary<string, bool> symbolPairs = new Dictionary<string, bool>();
+        #endregion
+
         #region others
         bool showOthers;
         #endregion
@@ -44,6 +56,9 @@ namespace MLib
         private void OnEnable()
         {
             titleStyle = new();
+            titleStyle.normal.textColor = Color.white;
+            titleStyle.fontSize = 20;
+            titleStyle.fontStyle = FontStyle.Bold;
             titleStyle.alignment = TextAnchor.MiddleCenter;
 
             animShowTimeScaleFields = new AnimBool(false);
@@ -54,16 +69,26 @@ namespace MLib
             animShowDataFields.valueChanged.AddListener(Repaint);
             styleDataText = new GUIStyle(EditorStyles.textArea);
             styleDataText.wordWrap = true;
+
+            buildTargetGroup = BuildTargetGroup.Android;
+            LoadDefineSymbols();
         }
         private void OnGUI()
         {
+            SetEditorDataConfig();
+
+            EditorGUILayout.Space(10f);
             SetEditorTimeScale();
 
             EditorGUILayout.Space(10f);
             SetEditorData();
 
             EditorGUILayout.Space(10f);
-            SetEditorPingResorucesFolder();
+            showOthers = EditorGUILayout.Foldout(showOthers, "Others");
+            if (showOthers)
+            {
+                SetEditorToggleDefineSymbols();
+            }
         }
 
         private void OnDisable()
@@ -86,6 +111,41 @@ namespace MLib
 
             return result;
         }
+
+        private void SetEditorDataConfig()
+        {
+            string editorConfigPath = GetEditorConfigPath();
+            soThis = new SerializedObject(this);
+            editorConfig = AssetDatabase.LoadAssetAtPath<EditorConfigSO>(editorConfigPath);
+
+            EditorGUILayout.LabelField("General");
+
+            if(editorConfig == null)
+            {
+                EditorGUILayout.HelpBox("Have to generate new Editor Config Asset", MessageType.Error);
+            }
+
+            SerializedProperty editorConfigProp = soThis.FindProperty("editorConfig");
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.PropertyField(editorConfigProp, true);
+            if (GUILayout.Button("Generate new asset"))
+            {
+                var instance = ScriptableObject.CreateInstance<EditorConfigSO>();
+                AssetDatabase.CreateAsset(instance, editorConfigPath);
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        private string GetEditorConfigPath()
+        {
+            MonoScript script = MonoScript.FromScriptableObject(this);
+            string scriptPath = AssetDatabase.GetAssetPath(script);
+            string dirPath = System.IO.Path.GetDirectoryName(scriptPath);
+
+            return $"{dirPath}/EditorConfig.asset";
+        }
+
         private void SetEditorTimeScale()
         {
             animShowTimeScaleFields.target = EditorGUILayout.ToggleLeft("Enable edit time scale in realtime", animShowTimeScaleFields.target);
@@ -141,21 +201,13 @@ namespace MLib
 
                     serializedData = JsonConvert.SerializeObject(localData, Formatting.Indented);
                 }
-            }
-
-            EditorGUILayout.EndFoldoutHeaderGroup();
-        }
-
-        private void SetEditorPingResorucesFolder()
-        {
-            showOthers = EditorGUILayout.Foldout(showOthers, "Others");
-            if (showOthers)
-            {
                 if (GUILayout.Button("OPEN SAVE FILE FOLDER EXPLORER"))
                 {
                     OpenFolder(Application.persistentDataPath);
                 }
             }
+
+            EditorGUILayout.EndFoldoutHeaderGroup();
         }
 
         private void OpenFolder(string path)
@@ -176,5 +228,113 @@ namespace MLib
             }
         }
 
+
+        private void SetEditorToggleDefineSymbols()
+        {
+            EditorGUILayout.LabelField("---Define symbols---", titleStyle);
+            if (!editorConfig)
+            {
+                EditorGUILayout.HelpBox("Have to generate new Editor Config Asset", MessageType.Error);
+                return;
+            }
+
+            buildTargetGroup = (BuildTargetGroup) EditorGUILayout.EnumFlagsField("Build Target", buildTargetGroup);
+
+            foreach (var symbol in symbolPairs.ToList())
+            {
+                bool value = symbol.Value;
+                symbolPairs[symbol.Key] = EditorGUILayout.ToggleLeft(symbol.Key, value);
+            }
+
+            if (GUILayout.Button("Reload Custom Symbols"))
+            {
+                LoadDefineSymbols();
+            }
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.HelpBox("This action may take some time.", MessageType.None);
+            if (GUILayout.Button("Apply"))
+            {
+                SaveDefineSymbols();
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        private void LoadDefineSymbols()
+        {
+            if (!editorConfig)
+                return;
+
+            symbolPairs = new();
+            foreach (string symbol in editorConfig.DefineSymbolsToggle)
+                symbolPairs.TryAdd(symbol, false);
+
+            string strSymbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
+            int count = 0;
+            string curSymbol = string.Empty;
+            while(count < strSymbols.Length)
+            {
+                char c = strSymbols[count];
+                count++;
+
+                if (!c.Equals(';'))
+                {
+                    curSymbol += c;
+                    
+                    if(count < strSymbols.Length) continue;
+                }
+
+                if (symbolPairs.ContainsKey(curSymbol))
+                {
+                    symbolPairs[curSymbol] = true;
+                }
+
+                // clear curSymbol
+                curSymbol = string.Empty;
+
+            }
+        }
+
+        private void SaveDefineSymbols()
+        {
+            if (!editorConfig)
+                return;
+
+            HashSet<string> setSymbols = new();
+            string strSymbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
+            int count = 0;
+            string curSymbol = string.Empty;
+            while (count < strSymbols.Length)
+            {
+                char c = strSymbols[count];
+                count++;
+
+                if (!c.Equals(';'))
+                {
+                    curSymbol += c;
+
+                    if (count < strSymbols.Length) continue;
+                }
+                
+                setSymbols.Add(curSymbol);
+
+                // clear curSymbol
+                curSymbol = string.Empty;
+
+            }
+
+            foreach (var pair in symbolPairs)
+            {
+                if(pair.Value == true)
+                    setSymbols.Add(pair.Key);
+                else if(pair.Value == false && setSymbols.Contains(pair.Key)) 
+                    setSymbols.Remove(pair.Key);
+            }
+
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(
+                targetGroup: buildTargetGroup,
+                defines: setSymbols.ToArray());
+
+            CompilationPipeline.RequestScriptCompilation();
+        }
     }
 }
